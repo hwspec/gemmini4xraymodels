d74989bfae0f4d9cea3ed07f728a88a84f4776c0
Hoku Ishibe <me@hokuishi.be>
2026-09-13 23:27:42 -0500

    turn on has_normalizations

--- src/main/scala/gemmini/GemminiConfigs.scala
+++ src/main/scala/gemmini/GemminiConfigs.scala
@@ -89,7 +89,7 @@ case class GemminiArrayConfig[T <: Data : Arithmetic, U <: Data, V <: Data](
                                                                              has_max_pool: Boolean = true,
                                                                              has_nonlinear_activations: Boolean = true,
                                                                              has_dw_convs: Boolean = true,
-                                                                             has_normalizations: Boolean = false,
+                                                                             has_normalizations: Boolean = true,
                                                                              has_first_layer_optimizations: Boolean = true,
                                                                              has_loop_conv: Boolean = true,
 
